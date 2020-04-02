9b04c733b37e8248e688fceaf80a927d76a72065
Brian Cochran <bcbriancochran@gmail.com>
2020-04-01 20:21:19 -0500

    Created Matrix4x4 class

---
+++ src/linearalgebra/Matrix4x4.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linearalgebra;
+
+/**
+ *
+ * @author Brian Cochran
+ */
+public class Matrix4x4 {
+    
+}
