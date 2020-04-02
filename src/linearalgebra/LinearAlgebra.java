f2b1f40b898e5e20b32f2043f73c9455ecd79152
Brian Cochran <bcbriancochran@gmail.com>
2020-04-01 22:34:06 -0500

    Removed main methods from Vector4D and Matrix4x4 classes and added main method to LinearAlgebra class

--- src/linearalgebra/LinearAlgebra.java
+++ src/linearalgebra/LinearAlgebra.java
@@ -15,7 +15,21 @@ public class LinearAlgebra {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-}
+        Vector4D vectorA = new Vector4D();
+        vectorA.set(3, 1);
+        Vector4D vectorB = new Vector4D();
+        vectorB.set(3, 1);
+        
+        System.out.println("a: " + vectorA);
+        System.out.println("b: " + vectorB);
+        System.out.println("a * b: " + vectorA.dotProduct(vectorB));
+        
+        Matrix4x4 matrixA = new Matrix4x4();
+        Matrix4x4 matrixB = new Matrix4x4();
+        Matrix4x4 matrixC = matrixA.multiply(matrixB);
+        
+        System.out.println("a: " + matrixA);
+        System.out.println("b: " + matrixB);
+        System.out.println("a * b: " + matrixC);
+    } // main(String[])
+} // LinearAlgebra
